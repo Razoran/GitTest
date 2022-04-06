b19a28bdc2ad9f25ea73972a526aeac3052ad86d
Razoran <iamrealmiro@gmail.com>
2022-04-06 10:54:22 +0300

    Updated GitTest

--- GitTest/src/com/GitTest/test1/main/main.java
+++ GitTest/src/com/GitTest/test1/main/main.java
@@ -3,5 +3,10 @@ package com.GitTest.test1.main;
 public class main {
     public static void  main(String[] args) {
         System.out.println("GitTest");
+        Type("new things");
+    }
+
+    public static void Type(String str) {
+        System.out.println(str);
     }
 }
